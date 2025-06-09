6a223993af1f3456c95d5200d6976a9bf8e29fd1
ranokhassanova <khasanova.rano@gmail.com>
2025-06-09 18:43:43 +0500

    add: admin dashboard page (uncompleted)

---
+++ app/admin/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'> 
+        <header className='admin-header'>
+            <Link href='/' className='cursor-pointer'>
+                <Image
+                    src="/assets/icons/logo-full.svg"
+                    height={32}
+                    width={32}
+                    alt='Logo'
+                    className='h-8 w-fit'
+                />
+            </Link>
+            <p className="text-16-smibold">Admin Dashboard</p>
+        </header>
+        <main className='admin-main'>
+            <section className="w-full space-y-4">
+                <h1 className="header">Welcome, Admin 😀</h1>
+                <p className="text-dark-700">Start the day with managing new appointment</p>
+            </section>
+            <section className="admin-stat"></section>
+        </main>
+    </div>
+  )
+}
+
+export default Admin
